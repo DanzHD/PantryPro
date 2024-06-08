567aca084799eb26c0a20a63a121d9b4f21eed74
Daniel Duong <daniel.d.cow@gmail.com>
2024-06-09 00:18:51 +1000

    Created button component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,19 @@
 import "./styles/_index.scss"
 import Text from "./Components/Text/Text.tsx";
+import Button from "./Components/Button/Button.tsx";
 
 function App() {
 
-  return <Text italicize bold danger heading centered>Hello World</Text>
+  return (
+      <>
+        <Text italicize bold danger heading centered>Hello World</Text>
+        <div style={{display: "flex", justifyContent: "center", alignContent: 'center'}}>
+
+          <Button> Button </Button>
+        </div>
+      </>
+  )
+
 
 }
 

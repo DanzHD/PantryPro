81081d1960833239093007152f640d7fc6ee17c1
Daniel Duong <daniel.d.cow@gmail.com>
2024-07-02 18:06:29 +1000

    Added checkbox logic

--- client/src/pages/Dashboard/Table.tsx
+++ client/src/pages/Dashboard/Table.tsx
@@ -7,7 +7,7 @@ import TableSettings from "./TableSettings.tsx";
 
 import { Food } from "../../dto/FoodResponse.tsx";
 import {useAuthContext} from "../../Context/AuthContext/useAuthContext.tsx";
-import {useEffect, useState} from "react";
+import {ChangeEvent, useEffect, useState} from "react";
 import {getUserFood, getUserFoodCount} from "../../api/food.tsx";
 import cx from "classnames";
 
@@ -18,15 +18,9 @@ function Table() {
 
   const [foods, setFood] = useState<Food[] | undefined>()
   const [foodCount, setFoodCount] = useState(0)
-  const [pagedSelected, setPageSelected] = useState(0)
+  const [pageSelected, setPageSelected] = useState(0)
+  const [foodsChecked, setFoodsChecked] = useState(new Map<number, boolean>())
 
-  function onPageChange(page: number) {
-    getUserFood({ limit: ROWS_PER_PAGE, pageNumber: page, token: accessToken})
-      .then(foods => {
-        setFood(foods)
-      })
-    setPageSelected(page)
-  }
 
 
   useEffect(() => {
@@ -46,6 +40,36 @@ function Table() {
         setFood(foods)
       })
   }, [accessToken]);
+  function onPageChange(page: number) {
+    getUserFood({ limit: ROWS_PER_PAGE, pageNumber: page, token: accessToken})
+      .then(foods => {
+        setFood(foods)
+      })
+    setPageSelected(page)
+  }
+
+  function handleCheckChange(id: number) {
+    const updatedFoodCheckState = new Map(foodsChecked)
+    if (updatedFoodCheckState.get(id)) {
+      updatedFoodCheckState.delete(id)
+    } else {
+      updatedFoodCheckState.set(id, true)
+    }
+    setFoodsChecked(updatedFoodCheckState)
+  }
+
+  function handleSelectDeselectAll(event: ChangeEvent<HTMLInputElement>) {
+    if (event.target.checked) {
+      const allChecked = new Map(foods?.map(({ id }) => [id, true] ))
+      setFoodsChecked(allChecked)
+    } else {
+      setFoodsChecked(new Map())
+    }
+  }
+
+
+
+
 
   return (
     <div className="table">
@@ -64,22 +88,22 @@ function Table() {
         </div>
         <div className="pagination">
           {
-            pagedSelected !== 0 &&
-              <div onClick={() => onPageChange(pagedSelected - 1)} className="material-symbols-outlined pagination__index">arrow_back</div>
+            pageSelected !== 0 &&
+              <div onClick={() => onPageChange(pageSelected - 1)} className="material-symbols-outlined pagination__index">arrow_back</div>
           }
 
           {
             [...Array(Math.ceil(foodCount / ROWS_PER_PAGE)).keys()].map(i => {
               return (
-                <div key={i} onClick={() => onPageChange(i)} className={cx("pagination__index", {"selected": pagedSelected === i})}>
+                <div key={i} onClick={() => onPageChange(i)} className={cx("pagination__index", {"selected": pageSelected === i})}>
                   <Text>{i+1}</Text>
                 </div>
               )
             })
           }
           {
-            pagedSelected !== (Math.ceil(foodCount / ROWS_PER_PAGE) - 1) &&
-              <div onClick={() => onPageChange(pagedSelected + 1)} className="material-symbols-outlined pagination__index">chevron_right</div>
+            pageSelected !== (Math.ceil(foodCount / ROWS_PER_PAGE) - 1) &&
+              <div onClick={() => onPageChange(pageSelected + 1)} className="material-symbols-outlined pagination__index">chevron_right</div>
           }
         </div>
 
@@ -91,7 +115,7 @@ function Table() {
 
         <thead>
           <tr>
-            <th className="th__checkbox"><input type="checkbox"/></th>
+            <th className="th__checkbox"><input onChange={handleSelectDeselectAll} type="checkbox"/></th>
             <th><Text bold>Name</Text></th>
             <th><Text bold>Quantity</Text></th>
             <th><Text bold>Food Group</Text> </th>
@@ -102,8 +126,9 @@ function Table() {
           {
             foods?.map(food => {
               return (
-                <tr key={food.id}>
-                  <td><input type="checkbox"/></td>
+
+                <tr key={food.id} style={foodsChecked.get(food.id) ? {backgroundColor: "lightblue"} : {}}>
+                  <td><input onChange={() => handleCheckChange(food.id)} checked={!!foodsChecked.get(food.id)} type="checkbox"/></td>
                   <td><Text>{food.name}</Text></td>
                   <td><Text>{food.quantity}</Text></td>
                   <td><Text>{food.foodGroup}</Text></td>
